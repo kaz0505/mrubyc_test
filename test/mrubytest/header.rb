f4940d56395eea7fe2b5f1808b21979bd67688ac
Yutaka HARA <yhara@netlab.jp>
2016-11-16 17:46:44 +0900

    mrubytest: headerを簡略化した

--- test/mrubytest/header.rb
+++ test/mrubytest/header.rb
@@ -1,33 +1,27 @@
-$title = ""
-def assert(title, section)
-  $title = title
+def assert_true(given)
+  if given
+    puts "ok"
+  else
+    puts "ng"
+  end
+end
+
+def assert_false(given)
+  assert_true(!given)
 end
 
 def assert_equal(expected, given)
-  if expected == given
-    puts "ok: " + $title
-  else
-    puts "ng: " + $title
-  end
+  assert_true(expected == given)
 end
 
 def assert_not_equal(expected, given)
-  if expected != given
-    puts "ok: " + $title
-  else
-    puts "ng: " + $title
-  end
+  assert_true(expected != given)
 end
-
-def assert_true(given)
-  if given
-    puts "ok: " + $title
-  else
-    puts "ng: " + $title
-  end
-end
-def assert_false(given); assert_true(!given); end
 
 def assert_nil(arg)
   assert_true(nil == arg)
+end
+
+def assert_raise(arg)
+  # skip
 end
